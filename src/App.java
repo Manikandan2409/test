a5ee46d4bc05a1d06558bad614ea3c552fd2b66f
Manikandan B <mani-24164@mani-24164.csez.zohocorpin.com>
2025-07-31 11:02:05 +0530

    from appserver demo

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("This is from APP");
+        System.out.println("App.main");
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
