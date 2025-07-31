d3de13c8018aedc3d9eb35195d0bf6784aa9f90a
Manikandan B <mani-24164@mani-24164.csez.zohocorpin.com>
2025-07-31 11:10:50 +0530

    adding additional snippets in Main

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,26 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+        System.out.printf("Hello and welcome!\n");
 
-        for (int i = 1; i <= 5; i++) {
+        Animal animal = new Dog();
+        animal.sound();
+        animal = new Cat();
+        animal.sound();
 
-            System.out.println("i = " + i);
-        }
+    }
+}
+
+class Dog implements  Animal{
+    @Override
+    public void sound() {
+        System.out.println("bark");
+    }
+}
+
+class  Cat implements  Animal{
+    @Override
+    public void sound() {
+        System.out.println("meow");
     }
 }
